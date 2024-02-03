126a5a18e2cda6fad3cd047a9fb5375145060dd3
Apeloko Timilehin <110821276+haypeks@users.noreply.github.com>
2024-02-03 02:52:49 +0000

    updated

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*..................NAVBAR..................*/
 const primaryHeader = document.querySelector(".primary-header");
 const navbar = document.querySelector(".navbar");
 const navToggle = document.querySelector(".toggle-menu");
@@ -9,3 +10,31 @@ navToggle.addEventListener("click", () => {
   navbar.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
 });
+
+/*..................SIZES.................*/
+const size = document.querySelectorAll(".category");
+
+size.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    size.forEach((sizebtn) => {
+      sizebtn.classList.remove("selected");
+    });
+    e.target.classList.add("selected");
+  });
+});
+
+/*..................IMAGES.................*/
+const mainImage = document.querySelector("#main-image");
+const smallImage = document.querySelectorAll(".small-image");
+
+smallImage[0].onclick = function () {
+  mainImage.src = smallImage[0].src;
+};
+
+smallImage[1].onclick = function () {
+  mainImage.src = smallImage[1].src;
+};
+
+smallImage[2].onclick = function () {
+  mainImage.src = smallImage[2].src;
+};
